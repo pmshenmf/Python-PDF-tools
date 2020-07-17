84ef2fa973d8e4ee3bb969b8e6286c200238292d
pmshenmf <52160986+pmshenmf@users.noreply.github.com>
2020-07-17 10:52:41 +0800

    Update PDF工具GUI.py
    优化合并，可选择任意文件

--- PDF工具GUI.py
+++ PDF工具GUI.py
@@ -16,14 +16,13 @@ def split(doc, from_pa, to_pa):
     return s1
 
 #合并
-def merge(doc1,doc2):
-    Doc=doc1[0:-4]
-    doc1=fitz.open(doc1)
-    doc2=fitz.open(doc2)
-    doc3 = fitz.open()
-    doc3.insertPDF(doc1)
-    doc3.insertPDF(doc2)
-    doc3.save(Doc+"(合并).pdf")
+def merge(docs):
+    Doc=docs[0]
+    doc_merge=fitz.open()
+    for i in docs:
+        doc_temp=fitz.open(i)
+        doc_merge.insertPDF(doc_temp)
+    doc_merge.save(Doc+"(合并).pdf")
     #print("操作完成，文件以保存在:\n"+Doc+"(合并).pdf")
     m1="操作完成，文件以保存在:\n"+Doc+"(合并).pdf"
     return m1
